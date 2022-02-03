77a1e80643be94593c950ac43929807f4cff9067
Jannes <jannes.luca@hotmail.com>
2022-02-03 11:35:33 +0100

    menu reworked

--- Assets/Menu/SliderScript.cs
+++ Assets/Menu/SliderScript.cs
@@ -11,32 +11,28 @@ public class SliderScript : MonoBehaviour
     public Slider FOVSlider;
     public Slider SensivitySlider;
 
-    public TextMeshProUGUI MusicValue;
-    public TextMeshProUGUI SFXValue;
-    public TextMeshProUGUI FOVValue;
-    public TextMeshProUGUI SensivityValue;
-
+   
 
     private void Start()
     {
         MusicSlider.onValueChanged.AddListener((mvalue) =>
         {
-            MusicValue.text = ((int) (mvalue*100)).ToString() + " %";
+            
         });
 
         SFXSlider.onValueChanged.AddListener((svalue) =>
         {
-            SFXValue.text = ((int)(svalue * 100)).ToString() + " %";
+            
         });
 
         FOVSlider.onValueChanged.AddListener((fvalue) =>
         {
-            FOVValue.text = ((int) fvalue).ToString() + "°";
+            
         });
 
         SensivitySlider.onValueChanged.AddListener((value) =>
         {
-            SensivityValue.text = value.ToString("0.00");
+            
         });
     }
 }
